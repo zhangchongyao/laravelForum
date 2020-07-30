bacadccc77cc22f622504a3f8d4768d7f12dca75
tutuut <814146171@qq.com>
2020-07-30 12:01:37 +0800

    authorized access

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,11 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show']]);
+    }
+
     /**
      * 个人信息
      * @param User $user
@@ -26,11 +31,13 @@ class UsersController extends Controller
      */
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
     public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
+        $this->authorize('update', $user);
         $data = $request->all();
         if($request->avatar) {
             $result = $uploader->save($request->avatar, 'avatars', $user->id, 416);
