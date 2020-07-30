c43d0cff9674fb5e721b506e91bf69dab7aaa2cb
tutuut <814146171@qq.com>
2020-07-30 15:08:25 +0800

    topic list page

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -7,4 +7,22 @@ use Illuminate\Database\Eloquent\Model;
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    /**
+     * 话题和分类一对一关联
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    /**
+     * 话题和用户一对一关联
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
