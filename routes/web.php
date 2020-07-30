5ba46edfc4e85a6ce4fb6d542e6d239c235b2867
tutuut <814146171@qq.com>
2020-07-30 12:48:53 +0800

    generate topic skeleton

--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,5 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 
 //个人页面
 Route::resource('users', 'UsersController', ['only' => ['show','update','edit']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
