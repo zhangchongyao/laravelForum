16575acc3107dbcb25caa1feddd0041078adebcb
tutuut <814146171@qq.com>
2020-07-30 20:06:16 +0800

    active categories

--- app/helpers.php
+++ app/helpers.php
@@ -4,3 +4,11 @@ function route_class()
 {
     return str_replace('.', '-', Route::currentRouteName());
 }
+
+function category_nav_active($route)
+{
+    if(request()->url() === $route) {
+        return "active";
+    }
+    return null;
+}
