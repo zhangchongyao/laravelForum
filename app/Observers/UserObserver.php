b2fef541687cb43f7c2be8f843495502b75da57c
tutuut <814146171@qq.com>
2020-08-02 23:36:13 +0800

    user register default avatar

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -18,4 +18,12 @@ class UserObserver
     {
         //
     }
+
+    public function saving(User $user)
+    {
+        //这样写扩展性更高，只有空的时候才指定默认头像
+        if(empty($user->avatar)) {
+            $user->avatar = 'http://47.114.130.18/uploads/images/avatars/4wtaayzqGFjS6TfJCH96RSQb8laHloWM.jpg';
+        }
+    }
 }
