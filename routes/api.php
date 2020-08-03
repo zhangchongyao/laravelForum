c626c05cea44b397df194692bde6eca358ae32ed
tutuut <814146171@qq.com>
2020-08-03 14:18:03 +0800

    frequency limit

--- routes/api.php
+++ routes/api.php
@@ -18,11 +18,19 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function() {
-    //短信验证码
-    Route::post('verificationCodes', 'VerificationCodesController@store')
-        ->name('verificationCodes.store');
-    //用户注册
-    Route::post('users', 'UsersController@store')
-        ->name('users.store');
+Route::prefix('v1')->namespace('Api')
+    ->name('api.v1.')
+    ->group(function() {
+        Route::middleware('throttle:' . config('api.rate_limits.sign'))->group(function() {
+            //短信验证码
+            Route::post('verificationCodes', 'VerificationCodesController@store')
+                ->name('verificationCodes.store');
+            //用户注册
+            Route::post('users', 'UsersController@store')
+                ->name('users.store');
+        });
+
+        Route::middleware('throttle:' . config('api.rate_limits.access'))->group(function() {
+
+        });
 });
