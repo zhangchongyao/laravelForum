114b9bed7c206d5a9257c5e6f11d0376aeefd311
tutuut <814146171@qq.com>
2020-08-03 21:16:44 +0800

    image verification code

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,9 @@ Route::prefix('v1')->namespace('Api')
     ->name('api.v1.')
     ->group(function() {
         Route::middleware('throttle:' . config('api.rate_limits.sign'))->group(function() {
+            //图片验证码
+            Route::post('captchas', 'CaptchasController@store')
+                ->name('captchas.store');
             //短信验证码
             Route::post('verificationCodes', 'VerificationCodesController@store')
                 ->name('verificationCodes.store');
