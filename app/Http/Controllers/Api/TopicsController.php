2e3f067a17cdb0b9762fd90a0a9982a1730c903c
tutuut <814146171@qq.com>
2020-08-04 19:22:04 +0800

    topic update

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -9,14 +9,32 @@ use Illuminate\Http\Request;
 
 class TopicsController extends Controller
 {
-    //
-
+    /**
+     * 发布话题资源
+     * @param TopicRequest $request
+     * @param Topic $topic
+     * @return TopicResource
+     */
     public function store(TopicRequest $request, Topic $topic)
     {
         $topic->fill($request->all());
         $topic->user_id = $request->user()->id;
         $topic->save();
 
+        return new TopicResource($topic);
+    }
+
+    /**
+     * 修改话题资源
+     * @param TopicRequest $request
+     * @param Topic $topic
+     * @return TopicResource
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function update(TopicRequest $request, Topic $topic)
+    {
+        $this->authorize('update', $topic);
+        $topic->update($request->all());
         return new TopicResource($topic);
     }
 }
