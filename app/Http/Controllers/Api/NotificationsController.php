8274119bee0401f000b04d50a409827ca99cf07c
tutuut <814146171@qq.com>
2020-08-05 12:23:06 +0800

    message read

--- app/Http/Controllers/Api/NotificationsController.php
+++ app/Http/Controllers/Api/NotificationsController.php
@@ -19,10 +19,26 @@ class NotificationsController extends Controller
         return NotificationResource::collection($notifications);
     }
 
+    /**
+     * 未读消息统计
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function stats(Request $request)
     {
         return response()->json([
             'unread_count' => $request->user()->notification_count,
         ]);
+    }
+
+    /**
+     * 读取通知
+     * @param Request $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+     */
+    public function read(Request $request)
+    {
+        $request->user()->markAsRead();
+        return response(null, 204);
     }
 }
