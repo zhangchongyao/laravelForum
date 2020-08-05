efad83465ee59edc7f526d126be7d236ee0d4b3a
tutuut <814146171@qq.com>
2020-08-05 12:17:41 +0800

    message notification stats

--- app/Http/Controllers/Api/NotificationsController.php
+++ app/Http/Controllers/Api/NotificationsController.php
@@ -7,10 +7,22 @@ use App\Http\Resources\NotificationResource;
 
 class NotificationsController extends Controller
 {
+    /**
+     * 获取通知列表
+     * @param Request $request
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     */
     public function index(Request $request)
     {
         $notifications = $request->user()->notifications()->paginate();
 
         return NotificationResource::collection($notifications);
+    }
+
+    public function stats(Request $request)
+    {
+        return response()->json([
+            'unread_count' => $request->user()->notification_count,
+        ]);
     }
 }
